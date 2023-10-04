c8b57c6d27124da182736eb7c71010ca78f81942
Mateo Quintero <mateoquintero@tyba.com.co>
2023-10-03 20:46:57 -0500

    async loading example

--- pdp/src/App.jsx
+++ pdp/src/App.jsx
@@ -1,16 +1,24 @@
-import React from "react";
+import React, {Suspense, useState} from "react";
 import ReactDOM from "react-dom";
 
 import "./index.scss";
 
-import Header from "home/Header";
+const Header = React.lazy(()=> import("home/Header"));
 import Footer from "home/Footer";
 
-const App = () => (
+const App = () => {
+  const [showHeader, setShowHeader] = useState(false);
+  return (
   <div className="text-3xl mx-auto max-w-6xl">
-  <Header />
+    {showHeader && (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Header />
+    </Suspense>
+  )}
+  <button className="text-3xl p-5" onClick={() => setShowHeader(true)}>Show Header</button>
     <div className="my-10">Home Page Content</div>
     <Footer />
   </div>
 );
+  }
 ReactDOM.render(<App />, document.getElementById("app"));
