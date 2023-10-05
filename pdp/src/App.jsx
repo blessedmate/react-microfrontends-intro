882db097d1f88a2c0d8efb3c5eab0f3923a1069e
Mateo Quintero <mateoquintero@tyba.com.co>
2023-10-04 21:08:26 -0500

    add links

--- pdp/src/App.jsx
+++ pdp/src/App.jsx
@@ -1,33 +1,27 @@
-import React, {Suspense, useState} from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import "remixicon/fonts/remixicon.css";
 import "./index.scss";
 
-const Header = React.lazy(()=> import("home/Header"));
+import Header from "home/Header";
 import Footer from "home/Footer";
 import PDPContent from "./PDPContent";
 
-const App = () => {
-  const [showHeader, setShowHeader] = useState(false);
-  return (
-    <Router>
-      <div className="text-3xl mx-auto max-w-6xl">
-        {showHeader && (
-          <Suspense fallback={<div>Loading...</div>}>
-          <Header />
-        </Suspense>
-      )}
-      <button className="text-3xl p-5" onClick={() => setShowHeader(true)}>Show Header</button>
-        <div className="my-10">
-          <Routes>
-            <Route path="/product/:id" element={< PDPContent />} />
-          </Routes>
-        </div>
-        <Footer />
+const App = () =>
+(
+  <Router>
+    <div className="text-3xl mx-auto max-w-6xl">
+      <Header />
+      <div className="my-10">
+        <Routes>
+          <Route path="/product/:id" element={< PDPContent />} />
+        </Routes>
       </div>
+      <Footer />
+    </div>
   </Router>
 );
-  }
+
 ReactDOM.render(<App />, document.getElementById("app"));
